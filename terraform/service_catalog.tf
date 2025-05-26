3f06b1ad8fedf50acd72122c257c83ee18a8d5c6
Roberto Landa <robertolanda@Landa-2.local>
2025-05-25 23:05:01 -0500

    ADD: Política para restringir regiones

--- terraform/service_catalog.tf
+++
@@ -1,9 +0,0 @@
-# learn-terraform-aft-global-customizations/terraform/service_catalog.tf
-locals {
-  aft_management_account_id = "022748072614"  # Asegúrate de que este ID es correcto
-}
-
-resource "aws_servicecatalog_principal_portfolio_association" "learn_aft" {
-  portfolio_id  = "port-pfvpa5vdm5gxk"  # ID de tu portafolio nuevo
-  principal_arn = "arn:aws:iam::${local.aft_management_account_id}:role/AWSControlTowerExecution"
-}
